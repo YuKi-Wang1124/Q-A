d7648d4791a671df2b4fb8b5dec7723ccfd56bbc
yuki <wkiki1124@gmail.com>
2021-09-10 03:48:09 +0800

    commit

--- 問答APP/ViewController.swift
+++ 問答APP/ViewController.swift
@@ -6,14 +6,99 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionNumberLabel: UILabel!
+    
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    
+    var questions = Question.data
+    var index = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 每次打開 APP 會更改題目順序
+        questions.shuffle()
+        changeContents()
+        
     }
+    
+    func changeContents() {
+        questionLabel.text = questions[index].question
+        
+        var optionArray = [questions[index].optionA,
+                           questions[index].optionB,
+                           questions[index].optionC,
+                           questions[index].optionD]
+        // 更改四個選項的順序
+        optionArray.shuffle()
+       
+        // 設定 button title
+        for i in 0...3 {
+            // answerButtons[i].titleLabel?.font = UIFont(name: "", size: 36)
+            answerButtons[i].setTitle(optionArray[i], for: .normal)
+            answerButtons[i].tintColor = .black
+        }
+        questionNumberLabel.text = "\(index + 1) / 10"
+        scoreLabel.text = "\(score)"
+    }
+    
+    func restart() {
+            score = 0
+            index = 0
+            questionNumberLabel.text = "\(index + 1) / 10"
+            scoreLabel.text = "0"
+            questions.shuffle()
+            changeContents()
+        }
 
-
+    @IBAction func restartButtonAction(_ sender: Any) {
+        restart()
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        if sender.currentTitle == questions[index].rightAnswer {
+            score += 10
+        }
+        
+        index += 1
+       
+        if index < 10{
+            changeContents()
+        } else {
+            caculateScore()
+        }
+    }
+    
+    
+    func alert(title: String, message: String, buttonText: String) {
+        let alertController = UIAlertController (title: title, message: message, preferredStyle: .alert)
+        let alertButton = UIAlertAction(title: buttonText, style: .default, handler: {(_)in self.restart() })
+        alertController.addAction(alertButton)
+        self.present(alertController, animated: true, completion: nil)
+        }
+        
+        
+    func caculateScore() {
+        if score >= 80 {
+            alert(title: "總分 \(score) 分", message: "太厲害了！", buttonText: "再玩一次！")
+        } else if score >= 60 && score < 80 {
+            alert(title: "總分 \(score) 分", message: "很棒喔！", buttonText: "再玩一次！")
+        } else if score >= 30 && score < 60 {
+            alert(title: "總分 \(score) 分", message: "加油喔！", buttonText: "再玩一次！")
+        } else {
+            alert(title: "總分 \(score) 分", message: "還要再努力！", buttonText: "再玩一次！")
+            }
+        }
+        
+    
 }
 
